57b2106674f34ec1c1a64497ef0616edcf7f0cc9
bishal <46195146+bishal-dhakal@users.noreply.github.com>
2023-01-23 19:49:12 +0545

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+import colorama
+
+from colorama import Fore, Back,Style
+colorama.init(autoreset=True)
+
+
+print(Fore.BLUE+Back.YELLOW+ "Hey, I ma Shah Rukh Khan " +Fore.YELLOW+Back.BLUE+' And I am learning python')
